e73c3e746d203c331ed34e73205d5acb8c15d219
soyoung <soyoung.yang94@gmail.com>
2024-04-18 13:25:18 +0900

    delete team.js

--- project/team.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-    const hover = document.querySelector("#title .title");
-    alert(hover);
-    hover.setAttribute('text-decoration', 'none');
-    document.getElementById("title:active").setAttribute('text-decoration', 'none');
-    document.getElementById("title:focus").setAttribute('text-decoration', 'none');
-    document.getElementById("title:visited").setAttribute('text-decoration', 'none');
-});*/
