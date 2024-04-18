515dc6a9dab9d58455e6b962063e99a541ac2375
soyoung <soyoung.yang94@gmail.com>
2024-04-18 13:19:56 +0900

    design

---
+++ project/team.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    const hover = document.querySelector("#title .title");
+    alert(hover);
+    hover.setAttribute('text-decoration', 'none');
+    document.getElementById("title:active").setAttribute('text-decoration', 'none');
+    document.getElementById("title:focus").setAttribute('text-decoration', 'none');
+    document.getElementById("title:visited").setAttribute('text-decoration', 'none');
+});*/
